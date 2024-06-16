2c4e6867e8a0147d906435cce9119ed41d68a4c3
Kelsi <superigromen2018@gmail.com>
2024-06-16 17:40:51 +0300

    1.1.0.2

--- src/main/java/me/khealth/Listeners/playerListener.java
+++ src/main/java/me/khealth/Listeners/playerListener.java
@@ -8,10 +8,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import java.util.Collection;
+import org.jetbrains.annotations.NotNull;
 
 
 public class playerListener implements Listener {
@@ -30,12 +28,21 @@ public class playerListener implements Listener {
         double health = player.getHealth();
         Location location = player.getLocation().add(0, 1, 0);
         Location backLocation = player.getLocation().add(-1, 1, -1);
+        int x = player.getLocation().getBlockX();
+        int y = player.getLocation().getBlockY();
+        int z = player.getLocation().getBlockZ();
 
 
         if (health < 10.0) {
             Bukkit.getScheduler().runTaskLater(plugin, () -> {
                 player.sendBlockChange(location, Material.BARRIER.createBlockData());
                 player.setSwimming(true);
+                Material material = player.getWorld().getBlockAt(x,y,z).getType();
+                Location loc = player.getLocation();
+
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    player.sendBlockChange(loc, material.createBlockData());
+                }, 1);
             }, 2);
             Bukkit.getScheduler().runTaskLater(plugin, () -> {
                 player.addPotionEffect(PotionEffectType.DARKNESS.createEffect(100, 1));
